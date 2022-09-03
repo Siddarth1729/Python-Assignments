fe968d88307239edfded040ddf0cc32053dff30e
Siddarth1729 <91900419+Siddarth1729@users.noreply.github.com>
2022-09-04 01:57:30 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,29 @@
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+print("Hello Python")
+
+#5. Write a python script to print Hello on the first line and Python on the second line
+print("Hello\nPython")
+
+#6. Write a python script to print “MySirG” on the screen
+print("MySirG")
+
+#7. Write a python script to print “Teacher’s Day” on the screen
+print("Teacher's Day")
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data.
+name="Siddarth"
+print(name)
+
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name="Siddarth"
+age=21
+qualification="Under-graduate"
+years_of_Exp=2
+print(name)
+print(age)
+print(qualification)
+print(years_of_Exp)
+
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a=10 ;b=11; c=12
+print(a,'\n',b,'\n',c)
